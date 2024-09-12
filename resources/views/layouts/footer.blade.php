4e7209cb7c2efdbf667040437f199c82835c2d15
Rafi <abuahamedrafi@gmail.com>
2024-09-12 11:43:35 +0600

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>CSL Inventory | Powered by <a href="https://github.com/FahimAnzamDip/triangle-pos">Triangle POS</a> © {{ date('Y') }} </div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
